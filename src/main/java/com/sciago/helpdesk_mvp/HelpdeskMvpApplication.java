b618f10d2a45b50417b0baa45d6e66c248eda6bb
iago <iago.scandido3@gmail.com>
2026-01-02 20:33:04 -0300

    chore: initial Spring Boot setup with health check endpoint

---
+++ src/main/java/com/sciago/helpdesk_mvp/HelpdeskMvpApplication.java
@@ -0,0 +1,13 @@
+package com.sciago.helpdesk_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskMvpApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(HelpdeskMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sciago/helpdesk_mvp/HelpdeskMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sciago.helpdesk_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
